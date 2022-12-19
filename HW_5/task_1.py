ae581a537356c9d0aeba0af345dcf0942fa89967
Пользователь <n_babaeva@mail.ru>
2022-12-19 14:01:01 +0300

    дз 5

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -4,3 +4,18 @@
 Ввод: значение типа <str>
 Вывод: значение типа <str>
 """
+str_text = 'На автобус выпал снег'
+str_num = []
+
+value = str_text.split()
+for i in value:
+    if not ("а" in i and "б" in i and "в" in i):
+        str_num.append(i)
+
+result = ' '.join(str_num)
+print(result)
+
+def del_word_1(str_text):
+    return " ".join(filter(lambda x: not("а" in x and "б" in x and "в" in x), str_text.split()))
+
+print(del_word_1(str_text))
