0cb71fb029b376474a1192f909bf64901bd617fc
Пользователь <n_babaeva@mail.ru>
2022-12-20 11:09:08 +0300

    дз 6

--- HW_6/task_2.py
+++ HW_6/task_2.py
@@ -13,3 +13,23 @@
 [1, 3, 5]
 [1, 2, 5, 3, 10]
 """
+import random
+size = int(input('Введите размерность '))
+str_1 = [random.randint(1, 9) for i in range(size)]
+str_2 = []
+str_3 = []
+str_4 = []
+print(str_1)
+
+for i in str_1:
+    if str_1.count(i) == 1:
+        str_2.append(i)
+    elif i not in str_3:
+        str_3.append(i)
+    if i not in str_4:
+        str_4.append(i)
+    i += 1
+
+print(str_2)
+print(str_3)
+print(str_4)
